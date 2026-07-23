2ddf44be4caf4188713d071141f1f494931922b3
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:43:23 -0500

    Remove obsolete closing cost route array

--- src/routes/closingCostRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import { lazy } from "react";
-const TexasClosingCostCalculatorPage = lazy(() => import("@/pages/closingCosts/TexasClosingCostCalculatorPage"));
-export const closingCostRoutes = [{ path: "/texas-closing-cost-calculator", element: <TexasClosingCostCalculatorPage /> }];
