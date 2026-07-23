a8521312158cd8acaa7b82988df0321a5d429dcf
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:46:16 -0500

    Remove obsolete salary route array

--- src/routes/salaryRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasSalaryPage=lazy(()=>import("@/pages/income/TexasSalaryPage"));
-export const salaryRoutes=[{path:"/texas-salary-calculator",element:<TexasSalaryPage/>}];
