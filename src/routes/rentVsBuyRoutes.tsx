4baa6a0345261065febdfc4e7388ca74cd383d17
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:46:05 -0500

    Remove obsolete rent vs buy route array

--- src/routes/rentVsBuyRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasRentVsBuyPage=lazy(()=>import("@/pages/home/TexasRentVsBuyPage"));
-export const rentVsBuyRoutes=[{path:"/texas-rent-vs-buy-calculator",element:<TexasRentVsBuyPage/>}];
