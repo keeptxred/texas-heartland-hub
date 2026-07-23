92bab108c5063211115182f4369783d257cbc1af
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:45:17 -0500

    Remove obsolete mortgage calculator route array

--- src/routes/mortgageCalculatorRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import { lazy } from "react";
-const TexasMortgageCalculatorPage = lazy(() => import("@/pages/mortgage/TexasMortgageCalculatorPage"));
-export const mortgageCalculatorRoutes = [{ path: "/texas-mortgage-calculator", element: <TexasMortgageCalculatorPage /> }];
