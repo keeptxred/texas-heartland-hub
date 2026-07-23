fe1eacf4a4e5e44b31bd8b31d4b98301892eef92
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:43:40 -0500

    Remove obsolete cost of living route array

--- src/routes/costOfLivingRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import { lazy } from "react";
-const TexasCostOfLivingPage=lazy(()=>import("@/pages/cost/TexasCostOfLivingPage"));
-export const costOfLivingRoutes=[{path:"/texas-cost-of-living-calculator",element:<TexasCostOfLivingPage/>}];
