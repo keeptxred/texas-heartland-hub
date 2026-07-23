d5f5327d98dbb02b64b4edee049f3674ba05edc4
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:44:36 -0500

    Remove obsolete home affordability route array

--- src/routes/homeAffordabilityRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasHomeAffordabilityPage=lazy(()=>import("@/pages/homeAffordability/TexasHomeAffordabilityPage"));
-export const homeAffordabilityRoutes=[{path:"/texas-home-affordability-calculator",element:<TexasHomeAffordabilityPage/>}];
