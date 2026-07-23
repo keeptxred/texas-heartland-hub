5a7ff88154f8dce4a0ae4696120b7cb802c51447
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:44:09 -0500

    Remove obsolete equity growth route array

--- src/routes/equityGrowthRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import { lazy } from "react";
-
-const TexasEquityGrowthPage = lazy(() => import("@/pages/equity/TexasEquityGrowthPage"));
-
-export const equityGrowthRoutes = [
-  { path: "/texas-home-equity-growth-calculator", element: <TexasEquityGrowthPage /> },
-];
