3689e6f47ca1795ed72b24ff422b38995ced6801
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:43:54 -0500

    Remove obsolete down payment route array

--- src/routes/downPaymentRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasDownPaymentPage=lazy(()=>import("@/pages/downPayment/TexasDownPaymentPage"));
-export const downPaymentRoutes=[{path:"/texas-down-payment-calculator",element:<TexasDownPaymentPage/>}];
