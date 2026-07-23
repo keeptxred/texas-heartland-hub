29708a5e2aae0e5369734c3c037799a88a9605ca
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:45:29 -0500

    Remove obsolete mortgage payoff route array

--- src/routes/mortgagePayoffRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasMortgagePayoffPage=lazy(()=>import("@/pages/mortgage/TexasMortgagePayoffPage"));
-export const mortgagePayoffRoutes=[{path:"/texas-mortgage-payoff-calculator",element:<TexasMortgagePayoffPage/>}];
