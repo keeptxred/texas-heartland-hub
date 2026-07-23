40ba07609207d0d7224c0171c97c5bc3e9312d21
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:42:58 -0500

    Remove obsolete budget route array

--- src/routes/budgetRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasBudgetPlannerPage=lazy(()=>import("@/pages/budget/TexasBudgetPlannerPage"));
-export const budgetRoutes=[{path:"/texas-budget-planner",element:<TexasBudgetPlannerPage/>}];
