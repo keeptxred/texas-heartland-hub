5840da5f3c946979b4ed02baf4526759d2513ba9
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:45:50 -0500

    Remove obsolete refinance route array

--- src/routes/refinanceRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasRefinancePage=lazy(()=>import("@/pages/mortgage/TexasRefinancePage"));
-export const refinanceRoutes=[{path:"/texas-refinance-calculator",element:<TexasRefinancePage/>}];
