ea46a2ca482bf5ebe5260eb30469db3ce5fea10c
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:45:03 -0500

    Remove obsolete homeownership cost route array

--- src/routes/homeOwnershipCostRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasHomeOwnershipCostPage=lazy(()=>import("@/pages/home/TexasHomeOwnershipCostPage"));
-export const homeOwnershipCostRoutes=[{path:"/texas-home-ownership-cost-calculator",element:<TexasHomeOwnershipCostPage/>}];
