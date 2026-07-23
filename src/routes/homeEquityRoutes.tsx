096874624d13426de79750a418295ce4171c0f86
keeptxred <freddy.coppola@gmail.com>
2026-07-22 19:44:51 -0500

    Remove obsolete home equity route array

--- src/routes/homeEquityRoutes.tsx
+++
@@ -1,3 +0,0 @@
-import {lazy} from "react";
-const TexasHomeEquityPage=lazy(()=>import("@/pages/home/TexasHomeEquityPage"));
-export const homeEquityRoutes=[{path:"/texas-home-equity-calculator",element:<TexasHomeEquityPage/>}];
