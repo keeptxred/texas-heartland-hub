54f9b04d8905df89898e0307e5e4b90f6f3c008c
keeptxred <freddy.coppola@gmail.com>
2026-07-22 20:48:40 -0500

    Remove standalone mortgage impact route

--- src/routes/texas-mortgage-payment-impact-calculator.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import AdditionalCalculator from "@/components/calculators/AdditionalCalculator";
-
-export const Route = createFileRoute("/texas-mortgage-payment-impact-calculator")({
-  component: () => <AdditionalCalculator calculatorKey="mortgagePaymentImpact" />,
-});
