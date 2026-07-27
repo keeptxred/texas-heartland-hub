106c82cb1fefaa90ea0e08f96bcec093d1ce733e
keeptxred <freddy.coppola@gmail.com>
2026-07-26 19:09:27 -0500

    revert(routes): remove unsupported vehicle fee alias

--- src/routes/texas-vehicle-registration-fees-calculator.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/texas-vehicle-registration-fees-calculator")({
-  beforeLoad: () => {
-    throw redirect({ to: "/find-my-dmv", statusCode: 301 });
-  },
-});
