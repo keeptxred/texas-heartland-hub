737ace120f7ba0c22d80905870f87e3ec22e7a71
keeptxred <freddy.coppola@gmail.com>
2026-07-27 18:49:07 -0500

    Add voting location election identifiers

--- src/types/elections/identifiers.ts
+++ src/types/elections/identifiers.ts
@@ -25,6 +25,8 @@ export type CampaignFinanceReportId = ElectionIdentifier<"CampaignFinanceReportI
 export type CampaignFinanceReportSlug = ElectionIdentifier<"CampaignFinanceReportSlug">;
 export type IssuePositionId = ElectionIdentifier<"IssuePositionId">;
 export type IssuePositionSlug = ElectionIdentifier<"IssuePositionSlug">;
+export type VotingLocationId = ElectionIdentifier<"VotingLocationId">;
+export type VotingLocationSlug = ElectionIdentifier<"VotingLocationSlug">;
 export type RaceId = ElectionIdentifier<"RaceId">;
 export type RaceSlug = ElectionIdentifier<"RaceSlug">;
 export type CandidateId = ElectionIdentifier<"CandidateId">;
@@ -36,8 +38,8 @@ export type ForecastSlug = ElectionIdentifier<"ForecastSlug">;
 export type ElectionResultId = ElectionIdentifier<"ElectionResultId">;
 export type ElectionResultSlug = ElectionIdentifier<"ElectionResultSlug">;
 
-export type CoreElectionId = ElectionCycleId | OfficeId | DistrictId | CountyId | BallotMeasureId | ElectionEventId | EndorsementId | CampaignFinanceReportId | IssuePositionId | RaceId | CandidateId | PollId | ForecastId | ElectionResultId;
-export type CoreElectionSlug = ElectionCycleSlug | OfficeSlug | DistrictSlug | CountySlug | BallotMeasureSlug | ElectionEventSlug | EndorsementSlug | CampaignFinanceReportSlug | IssuePositionSlug | RaceSlug | CandidateSlug | PollSlug | ForecastSlug | ElectionResultSlug;
+export type CoreElectionId = ElectionCycleId | OfficeId | DistrictId | CountyId | BallotMeasureId | ElectionEventId | EndorsementId | CampaignFinanceReportId | IssuePositionId | VotingLocationId | RaceId | CandidateId | PollId | ForecastId | ElectionResultId;
+export type CoreElectionSlug = ElectionCycleSlug | OfficeSlug | DistrictSlug | CountySlug | BallotMeasureSlug | ElectionEventSlug | EndorsementSlug | CampaignFinanceReportSlug | IssuePositionSlug | VotingLocationSlug | RaceSlug | CandidateSlug | PollSlug | ForecastSlug | ElectionResultSlug;
 
 const SLUG_PATTERN = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
 
@@ -73,6 +75,7 @@ export const electionIds = {
   endorsement: (value: string) => toElectionIdentifier<"EndorsementId">(value, "Endorsement ID"),
   campaignFinanceReport: (value: string) => toElectionIdentifier<"CampaignFinanceReportId">(value, "Campaign finance report ID"),
   issuePosition: (value: string) => toElectionIdentifier<"IssuePositionId">(value, "Issue position ID"),
+  votingLocation: (value: string) => toElectionIdentifier<"VotingLocationId">(value, "Voting location ID"),
   race: (value: string) => toElectionIdentifier<"RaceId">(value, "Race ID"),
   candidate: (value: string) => toElectionIdentifier<"CandidateId">(value, "Candidate ID"),
   poll: (value: string) => toElectionIdentifier<"PollId">(value, "Poll ID"),
@@ -90,6 +93,7 @@ export const electionSlugs = {
   endorsement: (value: string) => toElectionSlug<"EndorsementSlug">(value, "Endorsement slug"),
   campaignFinanceReport: (value: string) => toElectionSlug<"CampaignFinanceReportSlug">(value, "Campaign finance report slug"),
   issuePosition: (value: string) => toElectionSlug<"IssuePositionSlug">(value, "Issue position slug"),
+  votingLocation: (value: string) => toElectionSlug<"VotingLocationSlug">(value, "Voting location slug"),
   race: (value: string) => toElectionSlug<"RaceSlug">(value, "Race slug"),
   candidate: (value: string) => toElectionSlug<"CandidateSlug">(value, "Candidate slug"),
   poll: (value: string) => toElectionSlug<"PollSlug">(value, "Poll slug"),
