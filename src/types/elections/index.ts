22c9e2c84dba0f28dac6780a7a3954de00fc6b77
keeptxred <freddy.coppola@gmail.com>
2026-07-27 16:41:38 -0500

    Export core ElectionRace model

--- src/types/elections/index.ts
+++ src/types/elections/index.ts
@@ -2,4 +2,5 @@ export * from "./domain";
 export * from "./identifiers";
 export * from "./metadata";
 export * from "./presentation";
+export * from "./race";
 export * from "./raceClassifications";
