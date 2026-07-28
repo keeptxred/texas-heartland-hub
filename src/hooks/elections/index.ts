5f5895b39f2b1c7b84236679483f86a2d93cdcf4
keeptxred <freddy.coppola@gmail.com>
2026-07-27 20:53:31 -0500

    Add polls by race hook

--- src/hooks/elections/index.ts
+++ src/hooks/elections/index.ts
@@ -3,5 +3,6 @@ export * from "./useElectionCandidate";
 export * from "./useElectionCandidates";
 export * from "./useElectionPolls";
 export * from "./useFeaturedElectionRaces";
+export * from "./usePollsByRace";
 export * from "./useElectionRace";
 export * from "./useElectionRaces";
