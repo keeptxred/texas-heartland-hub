11dc33dee1396a0710d081d2a5e836506a1242f2
keeptxred <freddy.coppola@gmail.com>
2026-08-03 18:09:04 -0500

    feat(search): export typo-tolerance utilities

--- src/shared/texas-platform/index.ts
+++ src/shared/texas-platform/index.ts
@@ -11,6 +11,7 @@ export * from './search-sharing';
 export * from './search-normalization';
 export * from './search-aliases';
 export * from './search-matching';
+export * from './search-fuzzy';
 export * from './adapters';
 export * from './providers';
 export * from './quality';
