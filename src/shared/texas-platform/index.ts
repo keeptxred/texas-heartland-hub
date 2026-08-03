ed372884424f14cc420b85258a7de8ae5fd91961
keeptxred <freddy.coppola@gmail.com>
2026-08-03 18:06:59 -0500

    chore: export shared search matching utilities

--- src/shared/texas-platform/index.ts
+++ src/shared/texas-platform/index.ts
@@ -10,6 +10,7 @@ export * from './search-params';
 export * from './search-sharing';
 export * from './search-normalization';
 export * from './search-aliases';
+export * from './search-matching';
 export * from './adapters';
 export * from './providers';
 export * from './quality';
