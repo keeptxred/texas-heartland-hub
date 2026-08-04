613e0fb8d5d41ec44afa68716a497cd7ed162e1d
keeptxred <freddy.coppola@gmail.com>
2026-08-04 14:41:38 -0500

    feat: export phase six governance analytics

--- src/shared/platform-core/index.ts
+++ src/shared/platform-core/index.ts
@@ -1,5 +1,6 @@
 export * from './contract';
 export * from './content-intelligence';
 export * from './publication-gate';
+export * from './governance-events';
 export * from './entities';
 export * from './fingerprint';
