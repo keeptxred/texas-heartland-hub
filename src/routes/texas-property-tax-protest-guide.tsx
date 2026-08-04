f66de69cfe9318b56b2cb9f9a43168f164738c09
keeptxred <freddy.coppola@gmail.com>
2026-08-04 18:43:19 -0500

    fix: redirect protest guide to TexasDefined canonical route

--- src/routes/texas-property-tax-protest-guide.tsx
+++ src/routes/texas-property-tax-protest-guide.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/texas-property-tax-protest-guide")({
   beforeLoad: ({ location }) => {
     throw redirect({
-      href: `https://texasdefined.com${location.pathname}${location.searchStr || ""}`,
+      href: `https://texasdefined.com/do/property-tax-protest${location.searchStr || ""}`,
       statusCode: 301,
     });
   },
