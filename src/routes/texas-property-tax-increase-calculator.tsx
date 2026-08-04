d0d67c4979ed6127d7796bb52716c3327c286159
keeptxred <freddy.coppola@gmail.com>
2026-08-04 18:43:28 -0500

    fix: redirect tax increase calculator to TexasDefined canonical route

--- src/routes/texas-property-tax-increase-calculator.tsx
+++ src/routes/texas-property-tax-increase-calculator.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/texas-property-tax-increase-calculator")({
   beforeLoad: ({ location }) => {
     throw redirect({
-      href: `https://texasdefined.com${location.pathname}${location.searchStr || ""}`,
+      href: `https://texasdefined.com/decide/property-taxes${location.searchStr || ""}`,
       statusCode: 301,
     });
   },
