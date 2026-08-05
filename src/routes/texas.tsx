03a36337a3198cd244ec3d1f7715654a392f8a41
keeptxred <freddy.coppola@gmail.com>
2026-08-04 19:43:54 -0500

    refactor: redirect Texas lifestyle namespace to TexasDefined

--- src/routes/texas.tsx
+++ src/routes/texas.tsx
@@ -1,5 +1,8 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/texas")({
-  component: () => <Outlet />,
+  beforeLoad: ({ location }) => {
+    const suffix = location.pathname === "/texas" ? "/texas-living" : location.pathname.replace(/^\/texas/, "");
+    throw redirect({ href: `https://texasdefined.com${suffix}${location.searchStr || ""}`, statusCode: 301 });
+  },
 });
