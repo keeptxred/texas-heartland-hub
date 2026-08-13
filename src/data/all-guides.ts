1ca1245c13a8498d9916a596e947472735b31445
keeptxred <freddy.coppola@gmail.com>
2026-08-13 18:27:42 -0500

    Register Texas driving law guides

--- src/data/all-guides.ts
+++ src/data/all-guides.ts
@@ -3,12 +3,14 @@ import { AGRICULTURE_SUPPORTING_GUIDES } from "@/data/supporting-guides-agricult
 import { VETERANS_SUPPORTING_GUIDES } from "@/data/supporting-guides-veterans";
 import { LAW_ENFORCEMENT_SUPPORTING_GUIDES } from "@/data/supporting-guides-law-enforcement";
 import { BORDER_SUPPORTING_GUIDES } from "@/data/supporting-guides-border";
+import { DRIVING_SUPPORTING_GUIDES } from "@/data/supporting-guides-driving";
 
 export const SUPPORTING_GUIDES = {
   ...AGRICULTURE_SUPPORTING_GUIDES,
   ...VETERANS_SUPPORTING_GUIDES,
   ...LAW_ENFORCEMENT_SUPPORTING_GUIDES,
   ...BORDER_SUPPORTING_GUIDES,
+  ...DRIVING_SUPPORTING_GUIDES,
 };
 
 export const ALL_GUIDES: Record<string, CornerstoneGuide> = {
