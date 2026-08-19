791b8a145aa12f9a165e693a864c334adac197e1
keeptxred <freddy.coppola@gmail.com>
2026-08-18 23:11:48 -0500

    Add Texas Case editorial detail route

---
+++ src/routes/texas-case.$slug.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { getTexasCasePosition } from "@/data/texas-case";
+import { TexasCasePositionPage, texasCasePositionHead } from "@/components/texas-case-position-page";
+
+export const Route = createFileRoute("/texas-case/$slug")({
+  loader: ({ params }) => {
+    const position = getTexasCasePosition(params.slug);
+    if (!position) throw notFound();
+    return { position };
+  },
+  head: ({ loaderData }) => loaderData?.position ? texasCasePositionHead(loaderData.position) : {},
+  component: TexasCasePositionRoute,
+});
+
+function TexasCasePositionRoute() {
+  const { position } = Route.useLoaderData();
+  return <TexasCasePositionPage position={position} />;
+}
