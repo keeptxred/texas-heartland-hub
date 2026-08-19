9c635debe128bf153cb2c468d2c0f80d184b146a
keeptxred <freddy.coppola@gmail.com>
2026-08-19 13:00:32 -0500

    Exclude retired static articles from image sitemap

--- src/routes/sitemap-images[.]xml.ts
+++ src/routes/sitemap-images[.]xml.ts
@@ -13,6 +13,7 @@ import {
 import { ARTICLES, isPublished } from "@/data/articles";
 import { listSitemapArticles } from "@/lib/evergreen.functions";
 import { getProducts } from "@/lib/products.functions";
+import { isStaticArticleIndexable } from "@/lib/static-article-indexability";
 
 const PRODUCT_CATALOG_LASTMOD = toIsoDate("2026-07-01T00:00:00-05:00");
 const TOMBSTONED_ARTICLE_SLUGS = new Set([
@@ -50,7 +51,9 @@ export const Route = createFileRoute("/sitemap-images.xml")({
           });
         };
 
-        for (const a of ARTICLES.filter((a) => isPublished(a))) {
+        for (const a of ARTICLES.filter(
+          (a) => isPublished(a) && isStaticArticleIndexable(a),
+        )) {
           if (TOMBSTONED_ARTICLE_SLUGS.has(a.slug)) continue;
           push(
             `${BASE_URL}/news/${a.slug}`,
