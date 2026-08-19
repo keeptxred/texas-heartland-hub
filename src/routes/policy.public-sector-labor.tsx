076f3f5e1b53f3b06a117abac9307ea9aadb82d8
keeptxred <freddy.coppola@gmail.com>
2026-08-19 15:39:17 -0500

    Add E-Verify, public-sector labor, and licensing mobility trackers (#776)
    * Add policy tracker wave 7 data
    
    * Add E-Verify employment policy route
    
    * Add public sector labor policy route
    
    * Add occupational licensing mobility policy route
    
    * Wire wave 7 policy trackers into hub

---
+++ src/routes/policy.public-sector-labor.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { POLICY_TRACKERS_WAVE7 } from "@/data/policy-trackers-wave7";
+import { PolicyTrackerPage, policyTrackerHead } from "@/components/policy-tracker-page";
+
+const tracker = POLICY_TRACKERS_WAVE7.find((item) => item.slug === "public-sector-labor")!;
+
+export const Route = createFileRoute("/policy/public-sector-labor")({
+  head: () => policyTrackerHead(tracker),
+  component: () => <PolicyTrackerPage tracker={tracker} />,
+});
