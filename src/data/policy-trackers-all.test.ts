bed684245d000bfec099c6b8f1683fcb93d47a19
keeptxred <freddy.coppola@gmail.com>
2026-08-19 17:25:30 -0500

    Extend policy registry regression through Wave 13

--- src/data/policy-trackers-all.test.ts
+++ src/data/policy-trackers-all.test.ts
@@ -13,6 +13,7 @@ import { POLICY_TRACKERS_WAVE9 } from "@/data/policy-trackers-wave9";
 import { POLICY_TRACKERS_WAVE10 } from "@/data/policy-trackers-wave10";
 import { POLICY_TRACKERS_WAVE11 } from "@/data/policy-trackers-wave11";
 import { POLICY_TRACKERS_WAVE12 } from "@/data/policy-trackers-wave12";
+import { POLICY_TRACKERS_WAVE13 } from "@/data/policy-trackers-wave13";
 
 const expectedCount = [
   POLICY_TRACKERS,
@@ -27,6 +28,7 @@ const expectedCount = [
   POLICY_TRACKERS_WAVE10,
   POLICY_TRACKERS_WAVE11,
   POLICY_TRACKERS_WAVE12,
+  POLICY_TRACKERS_WAVE13,
 ].reduce((sum, wave) => sum + wave.length, 0);
 
 describe("unified policy tracker registry", () => {
