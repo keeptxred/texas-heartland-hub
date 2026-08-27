75db742ae469b1d875058cbe073e09159f028cbf
keeptxred <freddy.coppola@gmail.com>
2026-08-27 14:31:36 -0500

    Point short moving checklist alias to current TexasDefined page

--- src/routes/moving-checklist.tsx
+++ src/routes/moving-checklist.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/moving-checklist")({
   beforeLoad: ({ location }) => {
-    throw redirect({ href: `https://texasdefined.com/moving-to-texas-checklist${location.searchStr || ""}`, statusCode: 301 });
+    throw redirect({ href: `https://texasdefined.com/moving-to-texas${location.searchStr || ""}`, statusCode: 301 });
   },
 });
