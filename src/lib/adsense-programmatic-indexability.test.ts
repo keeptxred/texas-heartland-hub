01ac06e408209e33f0a7e2e5db178aa1314c8b79
keeptxred <freddy.coppola@gmail.com>
2026-08-27 15:57:36 -0500

    Rebase wave 3 policy tracker readiness onto latest main (#1376)

--- src/lib/adsense-programmatic-indexability.test.ts
+++ src/lib/adsense-programmatic-indexability.test.ts
@@ -8,6 +8,7 @@ import { WAVE4_ISSUE_GUIDE_SLUGS } from "@/data/issue-guide-wave4-upgrades";
 import { ALL_POLICY_TRACKERS, getAnyPolicyTracker } from "@/data/policy-trackers-all";
 import { PRIORITY_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-upgrades";
 import { WAVE2_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave2-upgrades";
+import { WAVE3_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave3-upgrades";
 import { POLITICAL_SEARCH_GUIDES } from "@/data/political-search-guides";
 import { isIssueGuideIndexable } from "@/lib/issue-guide-indexability";
 import { isPolicyTrackerIndexable } from "@/lib/policy-tracker-indexability";
@@ -21,6 +22,7 @@ const referenceRoute = fs.readFileSync(new URL("../routes/texas-political-refere
 const EXPECTED_INDEXABLE_POLICY_TRACKER_SLUGS = [
   ...PRIORITY_INDEXABLE_POLICY_TRACKER_SLUGS,
   ...WAVE2_INDEXABLE_POLICY_TRACKER_SLUGS,
+  ...WAVE3_INDEXABLE_POLICY_TRACKER_SLUGS,
   "social-media-viewpoint-moderation",
 ];
 
