915d744f0b27b41a79c6a51ebd8112753b743e02
keeptxred <freddy.coppola@gmail.com>
2026-08-26 21:47:40 -0500

    Add Texas realignment guide to politics sitemap

--- src/routes/sitemap-political-figures[.]xml.ts
+++ src/routes/sitemap-political-figures[.]xml.ts
@@ -3,7 +3,7 @@ import type {} from "@tanstack/react-start";
 import { ALL_TEXAS_POLITICAL_FIGURES } from "@/data/texas-political-figures-all";
 import { BASE_URL, renderUrlset, toIsoDate, xmlResponse, type UrlEntry } from "@/lib/sitemap-shared";
 
-const LASTMOD = toIsoDate("2026-08-26T21:29:00-05:00");
+const LASTMOD = toIsoDate("2026-08-26T21:45:00-05:00");
 
 export const Route = createFileRoute("/sitemap-political-figures.xml")({
   server: {
@@ -11,6 +11,7 @@ export const Route = createFileRoute("/sitemap-political-figures.xml")({
       GET: async () => {
         const entries: UrlEntry[] = [
           { loc: `${BASE_URL}/texas-politics/figures`, lastmod: LASTMOD },
+          { loc: `${BASE_URL}/texas-politics/how-texas-became-republican`, lastmod: LASTMOD },
           ...ALL_TEXAS_POLITICAL_FIGURES.map((figure) => ({
             loc: `${BASE_URL}/texas-politics/figures/${figure.slug}`,
             lastmod: LASTMOD,
