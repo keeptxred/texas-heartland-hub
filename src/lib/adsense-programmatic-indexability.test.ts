e5a34793f3ab4d53f4b79a170f99ab61e2f300aa
keeptxred <freddy.coppola@gmail.com>
2026-08-29 10:36:44 -0500

    Extend programmatic indexability manifest through wave 12

--- src/lib/adsense-programmatic-indexability.test.ts
+++ src/lib/adsense-programmatic-indexability.test.ts
@@ -17,6 +17,7 @@ import { WAVE8_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave
 import { WAVE9_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave9-upgrades";
 import { WAVE10_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave10-upgrades";
 import { WAVE11_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave11-upgrades";
+import { WAVE12_INDEXABLE_POLICY_TRACKER_SLUGS } from "@/data/policy-tracker-wave12-upgrades";
 import { POLITICAL_SEARCH_GUIDES } from "@/data/political-search-guides";
 import { isIssueGuideIndexable } from "@/lib/issue-guide-indexability";
 import { isPolicyTrackerIndexable } from "@/lib/policy-tracker-indexability";
@@ -39,6 +40,7 @@ const EXPECTED_INDEXABLE_POLICY_TRACKER_SLUGS = [
   ...WAVE9_INDEXABLE_POLICY_TRACKER_SLUGS,
   ...WAVE10_INDEXABLE_POLICY_TRACKER_SLUGS,
   ...WAVE11_INDEXABLE_POLICY_TRACKER_SLUGS,
+  ...WAVE12_INDEXABLE_POLICY_TRACKER_SLUGS,
   "social-media-viewpoint-moderation",
 ];
 
