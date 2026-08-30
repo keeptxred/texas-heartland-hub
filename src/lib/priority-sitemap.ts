b1311511fbfbb9d76f290cd527e53fbcbf08556a
keeptxred <freddy.coppola@gmail.com>
2026-08-30 16:13:38 -0500

    Reject unowned priority news paths

--- src/lib/priority-sitemap.ts
+++ src/lib/priority-sitemap.ts
@@ -16,6 +16,8 @@ const DISALLOWED_PRIORITY_PATHS = new Set([
   "/bills/texas/89/hb/2746",
   "/bills/texas/89/hb/3435",
   "/bills/texas/89/hb/3913",
+  "/news/democrat-viral-tweet-falsely-claims-texas-congressional-map-eliminates-black-and-latino-majority-districts",
+  "/news/texas-lawmakers-propose-expanding-death-penalty-to-cover-abortion-after-fetal-heartbeat",
 ]);
 
 function isDisallowedPriorityPath(path: string) {
