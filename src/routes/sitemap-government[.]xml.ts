8131e28ba68a53c7b9ccd44697a3c74b390744e0
keeptxred <freddy.coppola@gmail.com>
2026-09-08 16:40:05 -0500

    Add judicial conduct guide to government sitemap

--- src/routes/sitemap-government[.]xml.ts
+++ src/routes/sitemap-government[.]xml.ts
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 import { TEXAS_COURTS_REVIEWED } from "@/data/texas-courts-authority";
+import { SCJC_REVIEWED } from "@/data/texas-judicial-conduct-authority";
 import { GOVERNMENT_ENTITIES, GOVERNMENT_REVIEWED_AT, governmentPath } from "@/lib/texas-government";
 import { getPublicationGovernmentEntities } from "@/lib/government-entity-publication";
 import { isGovernmentEntityIndexable } from "@/lib/government-entity-indexability";
@@ -14,6 +15,7 @@ export const Route = createFileRoute("/sitemap-government.xml")({
       GET: async () => xmlResponse(renderUrlset([
         { loc: `${BASE_URL}/texas-government`, lastmod: toIsoDate(GOVERNMENT_REVIEWED_AT) },
         { loc: `${BASE_URL}/texas-courts`, lastmod: toIsoDate(TEXAS_COURTS_REVIEWED) },
+        { loc: `${BASE_URL}/texas-government/state-commission-on-judicial-conduct`, lastmod: toIsoDate(SCJC_REVIEWED) },
         ...INDEXABLE_GOVERNMENT_ENTITIES.map((entity) => ({
           loc: `${BASE_URL}${governmentPath(entity.slug)}`,
           lastmod: toIsoDate(GOVERNMENT_REVIEWED_AT),
