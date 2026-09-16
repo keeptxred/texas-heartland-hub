43ff077dfd4ee4bcbf8e0a0bbd193fc4e775041c
keeptxred <freddy.coppola@gmail.com>
2026-09-16 16:16:07 -0500

    Point new-resident handoff to exact TexasDefined guide

--- src/routes/vehicles.new-residents.tsx
+++ src/routes/vehicles.new-residents.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/vehicles/new-residents")({
   beforeLoad: ({ location }) => {
     throw redirect({
-      href: `https://texasdefined.com/texas-vehicle-registration${location.searchStr || ""}`,
+      href: `https://texasdefined.com/find-my-dmv${location.searchStr || ""}`,
       statusCode: 301,
     });
   },
