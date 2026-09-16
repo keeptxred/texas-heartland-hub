5eb1d7de656de49635b9ef72e02f11ad6829b231
keeptxred <freddy.coppola@gmail.com>
2026-09-16 16:16:18 -0500

    Canonicalize new-resident links to exact guide

--- src/lib/canonical-internal-redirects.ts
+++ src/lib/canonical-internal-redirects.ts
@@ -13,7 +13,7 @@ const INTERNAL_REDIRECT_CANONICALS: Readonly<Record<string, string>> = {
   "/find-my-dmv": "https://texasdefined.com/find-my-dmv",
   "/dmv": "https://texasdefined.com/texas-dmv",
   "/vehicles/registration": "https://texasdefined.com/texas-vehicle-registration",
-  "/vehicles/new-residents": "https://texasdefined.com/texas-vehicle-registration",
+  "/vehicles/new-residents": "https://texasdefined.com/find-my-dmv",
   "/vehicles/renewal": "https://texasdefined.com/texas-vehicle-registration-renewal",
   "/vehicles/registration-fees-taxes": "https://texasdefined.com/texas-vehicle-registration-fees-taxes",
   "/texas-property-tax-protest-guide": "https://texasdefined.com/do/property-tax-protest",
