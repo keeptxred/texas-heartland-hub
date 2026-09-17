e8aae26fca23db33040be23fd26185910ebf236a
keeptxred <freddy.coppola@gmail.com>
2026-09-17 11:21:16 -0500

    Redirect property-tax landing to TexasDefined

--- src/routes/property-taxes.tsx
+++ src/routes/property-taxes.tsx
@@ -2,6 +2,9 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/property-taxes")({
   beforeLoad: ({ location }) => {
-    throw redirect({ href: `/news/texas-property-tax-guide${location.searchStr || ""}`, statusCode: 301 });
+    throw redirect({
+      href: `https://texasdefined.com/learn/property-taxes${location.searchStr || ""}`,
+      statusCode: 301,
+    });
   },
 });
