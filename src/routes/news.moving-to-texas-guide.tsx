b2314015fc4d883b8c93051d14c53fb3c2367f7e
keeptxred <freddy.coppola@gmail.com>
2026-09-21 10:05:25 -0500

    Make moving-to-Texas guide a query-preserving handoff

--- src/routes/news.moving-to-texas-guide.tsx
+++ src/routes/news.moving-to-texas-guide.tsx
@@ -1,12 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
-const TARGET_URL =
-  "https://texasdefined.com/article/moving-to-texas-what-nobody-tells-you";
-
 export const Route = createFileRoute("/news/moving-to-texas-guide")({
-  beforeLoad: () => {
+  beforeLoad: ({ location }) => {
     throw redirect({
-      href: TARGET_URL,
+      href: `https://texasdefined.com/article/moving-to-texas-what-nobody-tells-you${location.searchStr || ""}`,
       statusCode: 301,
       reloadDocument: true,
     });
