0c744272b4392b8db5c13abf8420dbcff5098780
keeptxred <freddy.coppola@gmail.com>
2026-09-23 14:13:57 -0500

    Preserve query parameters on src/routes/texas-resources.journey.$journeyId.tsx handoff

--- src/routes/texas-resources.journey.$journeyId.tsx
+++ src/routes/texas-resources.journey.$journeyId.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/texas-resources/journey/$journeyId")({
   beforeLoad: ({ params, location }) => {
     throw redirect({
-      href: `https://texasdefined.com/texas-resources?q=${encodeURIComponent(params.journeyId)}${location.searchStr ? `&${location.searchStr.replace(/^\\?/, "")}` : ""}`,
+      href: `https://texasdefined.com/texas-resources?q=${encodeURIComponent(params.journeyId)}${location.searchStr ? `&${location.searchStr.replace(/^\?/, "")}` : ""}`,
       statusCode: 301,
     });
   },
