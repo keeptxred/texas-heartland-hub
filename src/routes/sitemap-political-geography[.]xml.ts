adc5cc3e977b373780b3a4d1708979a638a1645b
keeptxred <freddy.coppola@gmail.com>
2026-09-24 21:48:11 -0500

    Add competitiveness guide to political geography sitemap

--- src/routes/sitemap-political-geography[.]xml.ts
+++ src/routes/sitemap-political-geography[.]xml.ts
@@ -4,15 +4,22 @@ import { BASE_URL, renderUrlset, toIsoDate, xmlResponse, type UrlEntry } from "@
 import { TEXAS_POLITICAL_GEOGRAPHY_AUTHORITY_PAGES } from "@/data/texas-political-geography-authority";
 
 const LASTMOD = toIsoDate("2026-08-30T16:15:00-05:00");
+const COMPETITIVENESS_LASTMOD = toIsoDate("2026-09-24T21:00:00-05:00");
 
 export const Route = createFileRoute("/sitemap-political-geography.xml")({
   server: {
     handlers: {
       GET: () => {
-        const entries: UrlEntry[] = TEXAS_POLITICAL_GEOGRAPHY_AUTHORITY_PAGES.map((page) => ({
-          loc: `${BASE_URL}/texas-politics/${page.slug}`,
-          lastmod: LASTMOD,
-        }));
+        const entries: UrlEntry[] = [
+          ...TEXAS_POLITICAL_GEOGRAPHY_AUTHORITY_PAGES.map((page) => ({
+            loc: `${BASE_URL}/texas-politics/${page.slug}`,
+            lastmod: LASTMOD,
+          })),
+          {
+            loc: `${BASE_URL}/texas-politics/why-texas-is-politically-competitive`,
+            lastmod: COMPETITIVENESS_LASTMOD,
+          },
+        ];
         return xmlResponse(renderUrlset(entries));
       },
     },
